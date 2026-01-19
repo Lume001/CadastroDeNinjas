bb270b00ee7b3d1010c250988cf3ec67dc0647e4
Lume001 <Amarinho728@gmail.com>
2026-01-19 19:02:19 -0300

    Criada a entidade NinjaModel

--- src/main/java/dev/java10x/CadastroDeNinja/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/NinjaController.java
@@ -1,13 +1,12 @@
 package dev.java10x.CadastroDeNinja;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //é um controlador para API rest
 @RequestMapping //faz um mapa de todas as rotas
-public class Controller {
+public class NinjaController {
 
 
     @GetMapping("/boasvindas")//pega informações
--- src/main/java/dev/java10x/CadastroDeNinja/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/NinjaModel.java
@@ -1,4 +1,48 @@
 package dev.java10x.CadastroDeNinja;
 
+import jakarta.persistence.*;
+
+@Entity// Entity transforma uma classe em uma entidade do BD
+@Table(name = "tb_cadastro")
 public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
