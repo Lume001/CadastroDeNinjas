adfa64716c2551bccfc67373683183347aca4146
Lume001 <Amarinho728@gmail.com>
2026-01-19 18:22:37 -0300

    Criado a controller

---
+++ src/main/java/dev/java10x/CadastroDeNinja/Controller.java
@@ -0,0 +1,20 @@
+package dev.java10x.CadastroDeNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //é um controlador para API rest
+@RequestMapping //faz um mapa de todas as rotas
+public class Controller {
+
+
+    @GetMapping("/boasvindas")//pega informações
+    public String boasVindas() {
+        return"Essa é minha primeira mensagem nessa rota";
+    }
+
+
+
+}
